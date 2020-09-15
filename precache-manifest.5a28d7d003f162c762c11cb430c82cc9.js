a1a6b400b939f9a145cce096d5879e2a0efbcb00
paschal <okwuosahpaschal@gmail.com>
2020-09-15 02:09:09 +0000

    Updates

---
+++ precache-manifest.5a28d7d003f162c762c11cb430c82cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbf23119fe1530a96c373c51024e0d0",
+    "url": "/tracenews/index.html"
+  },
+  {
+    "revision": "fd5ce50b9a3257b2db6f",
+    "url": "/tracenews/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "9e537d0457c7db96f068",
+    "url": "/tracenews/static/css/main.9a1be715.chunk.css"
+  },
+  {
+    "revision": "fd5ce50b9a3257b2db6f",
+    "url": "/tracenews/static/js/2.50e88130.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tracenews/static/js/2.50e88130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e537d0457c7db96f068",
+    "url": "/tracenews/static/js/main.f6f532b5.chunk.js"
+  },
+  {
+    "revision": "6849cee5e57a036cb9ad",
+    "url": "/tracenews/static/js/runtime-main.1a30cd0a.js"
+  }
+]);
